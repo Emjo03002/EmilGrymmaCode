9138effd3c510ac98f04acdad4dd53437040277c
Emjo03002 <70193931+Emjo03002@users.noreply.github.com>
2020-09-23 16:33:52 +0200

    Grymm Code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emil_Jönsson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
